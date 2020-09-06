d3f6046df699eb9b3c197a1a774f1c20a663faa2
CesarAugusto <cesaraugusto.ls@hotmail.com>
2020-09-06 16:02:09 -0300

    add navigation/stack

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <View></View>
+    </NavigationContainer>
+  );
+};
+
+export default App;
